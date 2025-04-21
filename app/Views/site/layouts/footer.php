189b15272561313b3821c4173ddc43a8c43f0550
itshenrii <henmucholindo@gmail.com>
2025-04-21 21:42:30 +0100

    tela home,etc

---
+++ app/Views/site/layouts/footer.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ZoShow</title>
+    <link rel="stylesheet" href="/public/assets/css/layouts.css">
+</head>
+<body>
+    <footer class="footer">
+        <div class="container">
+            <div class="footer-logo">
+                <img src="/public/assets/images/logozoshowCortadabranco.png" alt="ZoShow Logo" class="logo-img">
+            </div>
+            
+            <div class="copyright">
+                &copy; Copyright 2000-2025 ZoShow
+            </div>
+        </div>
+    </footer>
+</body>
+</html>
